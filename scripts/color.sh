748ab4ad23ac44727f409fd4888c9617e96eea8f
torjc01 <julio_cesar.torres@sct.gouv.qc.ca>
2022-07-11 17:23:45 -0400

    [func] script definicao de cores
    Signed-off-by: torjc01 <julio_cesar.torres@sct.gouv.qc.ca>

---
+++ scripts/color.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Nome script : color.sh
+# Objetivo    : Define variaveis de cores 
+# Data        : 11/07/2022
+# Programador : Julio Torres 
+# Target      : Raspberry PI OS
+#
+RED=`tput setaf 1`  
+GREEN=`tput setaf 2`
+BLUE=`tput setaf 4`
+BOLD=`tput bold`
+RESET=`tput sgr0`
